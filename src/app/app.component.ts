aa620e9ef0f2d7a0c091be84aa3ac4e58198188c
Oscar de La Rosa <oscardelarosa100@gmail.com>
2020-01-14 08:34:37 -0600

    v3: clase 11; todo funcionando de manera perfecta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Product } from './product.model'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,19 +8,61 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'platzi-store';
   items = ['nicolas', 'julian', 'perez'];
-  ejemplo = [1, 2, 3, 4, 5];
+
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/images/hoodie.png',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/images/mug.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/images/pin.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'assets/images/stickers1.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      image: 'assets/images/stickers2.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+
+  ]
+  
   addItem(){
     this.items.push('nuevo item');
   }
   deleteItem(index: number){
     this.items.splice(index, 1);
   }
-  addEjemplo(){
-    this.ejemplo.push('6');
-  }
-  deleteEjemplo(index: number){
-    this.ejemplo.splice(index, 1);
-  }
+
+
 }
 
 
