e9842957d1461da6f5bb6ed2755180c6e432c1a5
Oscar de La Rosa <oscardelarosa100@gmail.com>
2020-01-14 06:33:36 -0600

    v2: empiezo a utilizar estructuras de control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzi-store';
+  items = ['nicolas', 'julian', 'perez'];
+  ejemplo = [1, 2, 3, 4, 5];
+  addItem(){
+    this.items.push('nuevo item');
+  }
+  deleteItem(index: number){
+    this.items.splice(index, 1);
+  }
+  addEjemplo(){
+    this.ejemplo.push('6');
+  }
+  deleteEjemplo(index: number){
+    this.ejemplo.splice(index, 1);
+  }
 }
+
+
