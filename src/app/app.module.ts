79590b9b416126a4f91383ec47fe016ce8e6a9ce
Oscar de La Rosa <oscardelarosa100@gmail.com>
2020-01-14 10:06:52 -0600

    v5: todo perfecto, clase 13 finalizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ProductComponent } from './components/product.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
